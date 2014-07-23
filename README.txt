50929fd8703309951fc35292674f74e935ea1fce
Rishi Kulshreshtha <rishi.kulshreshtha@intelliswift.co.in>
2014-07-23 15:59:51 +0530

    Committing the module files.

---
+++ README.txt
@@ -0,0 +1,26 @@
+-- SUMMARY --
+
+Hovercard is a module which is based on hovercard, a free light weight jQuery plugin that enables you to display related information with the hovered label, link, or any html element of your choice. This module extends Drupal to provide Hovercard for the users of the website.
+
+  - Easy to install and use.
+  - Works on all user links with "username" as their class.
+  - Simply install it and see the magic!
+
+Special thanks to Prashant Chaudhary for such a charming jQuery Plugin https://github.com/prashantchaudhary/hovercard
+
+-- REQUIREMENTS --
+
+* None.
+
+-- INSTALLATION --
+
+* Install as usual, see https://www.drupal.org/node/1294804 for further information.
+
+-- CONFIGURATION --
+
+* None.
+
+-- CONTACT --
+
+Current maintainers:
+* Rishi B. Kulshreshtha (Rishi Kulshreshtha) - https://drupal.org/user/1403808
---
+++ js/hover_card.js
@@ -0,0 +1,34 @@
+(function($) {
+    Drupal.behaviors.hover_card = {
+        attach: function(context, settings) {
+            var modulePath = Drupal.settings.hover_card.path;
+            var hoverUserDetails = '<div class="hover-details"></div>';
+            $("a.username").hovercard({
+                detailsHTML: hoverUserDetails,
+                width: 250,
+                onHoverIn: function() {
+                    var user_id = $(this).find("a").attr("href").split("/");
+                    $.ajax({
+                        url: Drupal.settings.basePath + "hover-card",
+                        type: 'GET',
+                        data: {
+                            id: user_id[3],
+                        },
+                        beforeSend: function() {
+                            $(".hover-details").empty();
+                            $(".hover-details").prepend('<p style="text-align: center"><img src="' + modulePath + '/images/ajax-loader.gif"></p>');
+                        },
+                        success: function(data) {
+                            var obj = $.parseJSON(data);
+                            $(".hover-details").empty();
+                            $(".hover-details").html(obj.picture + '<strong>Username: </strong>' + obj.name + '<br/><strong>Email: </strong>' + obj.mail + '<br/><strong>Roles: </strong>' + obj.roles + '<br/>');
+                        },
+                        complete: function() {
+                            $('.loading-text').remove();
+                        }
+                    });
+                }
+            });
+        }
+    };
+})(jQuery);
