35a757e7f429cb119e37504c36e0d9979dad714a
Rishi Kulshreshtha <rishi.kulshreshtha@intelliswift.co.in>
2014-07-30 19:01:56 +0530

    Fixed changes according to http://pareview.sh/

--- templates/hover-card-template.tpl.php
+++ templates/hover-card-template.tpl.php
@@ -20,8 +20,8 @@
   !empty($user_picture) ?	print_r($user_picture) : "";
   ?>
   </div>
-    <div class="user-name"><strong>Username:</strong><?php print $details['name']; ?></div>
-    <div class="user-mail"><strong>Email:</strong><?php print $details['mail']; ?></div>
-    <div class="user-role"><strong>Roles:</strong><?php print $details['roles']; ?></div>
+    <div class="user-name"><strong>Username: </strong><?php print $details['name']; ?></div>
+    <div class="user-mail"><strong>Email: </strong><?php print $details['mail']; ?></div>
+    <div class="user-role"><strong>Roles: </strong><?php print $details['roles']; ?></div>
 </div>
 <?php drupal_exit(); ?>
