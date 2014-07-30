95127692dad7d80077cd2f5eb449fab185deaa25
Rishi Kulshreshtha <rishi.kulshreshtha@intelliswift.co.in>
2014-07-30 18:33:45 +0530

    Fixed changes according to https://www.drupal.org/node/2308911#comment-8997617

--- javascripts/hover_card.js
+++ javascripts/hover_card.js
@@ -1,39 +1,33 @@
-(function($) {
-    Drupal.behaviors.hover_card = {
-        attach: function(context, settings) {
-            var module_path = Drupal.settings.hover_card.path;
-            var hoverUserDetails = '<div class="hover-details"></div>';
-            $("a.username").hovercard({
-                detailsHTML: hoverUserDetails,
-                width: 250,
-                onHoverIn: function() {
-                    var user_id = $(this).find("a").attr("href").split("/");
-                    $.ajax({
-                        url: Drupal.settings.basePath + "hover-card",
-                        type: 'GET',
-                        data: {
-                            id: user_id[3],
-                        },
-                        beforeSend: function() {
-                            $(".hover-details").empty();
-                            $(".hover-details").prepend('<p style="text-align: center"><img src="' + module_path + '/images/ajax-loader.gif"></p>');
-                        },
-                        success: function(data) {
-                            var obj = $.parseJSON(data);
-                            $(".hover-details").empty();
-                            var result = "";
-                            if (obj.picture != null) {
-                                result += obj.picture;
-                            }
-                            result += '<strong>Username: </strong>' + obj.name + '<br/><strong>Email: </strong>' + obj.mail + '<br/><strong>Roles: </strong>' + obj.roles + '<br/>';
-                            $(".hover-details").html(result);
-                        },
-                        complete: function() {
-                            $('.loading-text').remove();
-                        }
-                    });
-                }
-            });
+(function ($) {
+  Drupal.behaviors.hover_card = {
+    attach: function (context, settings) {
+      var module_path = Drupal.settings.hover_card.module_path;
+      var hoverUserDetails = '<div class="hover-details"></div>';
+      $("a.username").hovercard({
+        detailsHTML: hoverUserDetails,
+        width: 250,
+        onHoverIn: function () {
+          var user_id = $(this).find("a").attr("href").split("/");
+          var hover_details = $(".hover-details");
+          $.ajax({
+            url: Drupal.settings.basePath + "hover-card",
+            type: 'GET',
+            data: {
+              id: user_id[3]
+            },
+            beforeSend: function () {
+              hover_details.empty();
+              hover_details.prepend('<p style="text-align: center"><img src="' + module_path + '/images/ajax-loader.gif"></p>');
+            },
+            success: function (data) {
+              hover_details.html(data);
+            },
+            complete: function () {
+              $('.loading-text').remove();
+            }
+          });
         }
-    };
+      });
+    }
+  };
 })(jQuery);
---
+++ templates/hover-card-template.tpl.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @file
+ * This template handles the layout of Hover Card.
+ *
+ * Variables available:
+ * - $details: An array of user details.
+ * - $details['name']: Use this to display the name of the user.
+ * - $details['mail']: Use this to display the registered e-mail of the user.
+ * - $details['picture']: Array which can be used to display picture of the user
+ * - $details['role']: Use this to display the role of the user.
+ *
+ */
+?>
+
+<div class="user-details">
+	<div class="user-image">
+	<?php
+	$user_picture = $details['picture'];
+	if (isset($user_picture) && !empty($user_picture)) {
+		print_r($user_picture);
+	}
+	?>
+	</div>
+    <div class="user-name"><strong>Username:</strong><?php print $details['name']; ?></div>
+    <div class="user-mail"><strong>Email:</strong><?php print $details['mail']; ?></div>
+    <div class="user-role"><strong>Roles:</strong><?php print $details['roles']; ?></div>
+</div>
+<?php drupal_exit(); ?>
