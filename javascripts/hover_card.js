6280554fd5104896a2c5b180e2d4b6b9f1291ac7
Rishi Kulshreshtha <rishi.kulshreshtha@intelliswift.co.in>
2014-08-01 15:10:35 +0530

    Fixed issues according to https://www.drupal.org/node/2308911#comment-8997617

--- javascripts/hover_card.js
+++ javascripts/hover_card.js
@@ -1,28 +1,25 @@
-(function ($) {
+(function($) {
   Drupal.behaviors.hover_card = {
-    attach: function (context, settings) {
-      var module_path = Drupal.settings.hover_card.module_path;
+    attach: function(context, settings) {
+      console.log(Drupal.settings.hover_card.name);
       var hoverUserDetails = '<div class="hover-details"></div>';
       $("a.username").hovercard({
         detailsHTML: hoverUserDetails,
         width: 250,
-        onHoverIn: function () {
-          var user_id = $(this).find("a").attr("href").split("/");
+        onHoverIn: function() {
+          var module_path = Drupal.settings.hover_card.module_path;
           var hover_details = $(".hover-details");
+          var user_id = $(this).find("a").attr("href").split("/");
           $.ajax({
-            url: Drupal.settings.basePath + "hover-card",
-            type: 'GET',
-            data: {
-              id: user_id[3]
-            },
-            beforeSend: function () {
+            url: Drupal.settings.basePath + "hover-card/" + user_id[3],
+            beforeSend: function() {
               hover_details.empty();
               hover_details.prepend('<p style="text-align: center"><img src="' + module_path + '/images/ajax-loader.gif"></p>');
             },
-            success: function (data) {
+            success: function(data) {
               hover_details.html(data);
             },
-            complete: function () {
+            complete: function() {
               $('.loading-text').remove();
             }
           });
--- templates/hover-card-template.tpl.php
+++ templates/hover-card-template.tpl.php
@@ -12,7 +12,6 @@
  * - $details['role']: Use this to display the role of the user.
  */
 ?>
-
 <div class="user-details">
   <div class="user-image">
   <?php
@@ -21,7 +20,7 @@
   ?>
   </div>
   <div class="user-name"><strong>Username: </strong><?php print $details['name']; ?></div>
-  <div class="user-mail"><strong>Email: </strong><?php print $details['mail']; ?></div>
+  <div class="user-mail"><strong>Email: </strong><a href="mailto:<?php print $details['mail']; ?>"><?php print $details['mail']; ?></a></div>
   <div class="user-role"><strong>Roles: </strong><?php print $details['roles']; ?></div>
 </div>
 <?php drupal_exit(); ?>
