94e89c58abfa87efd22132a3488f8dbe81ffe3e7
Rishi Kulshreshtha <rishishreshtha@gmail.com>
2014-08-09 00:11:10 +0530

    Fixed http://pareview.sh/

--- javascripts/hover_card.js
+++ javascripts/hover_card.js
@@ -6,11 +6,11 @@
 (function($) {
   Drupal.behaviors.hover_card = {
     attach: function(context, settings) {
-      var hoverUserDetails = '<div class="hover-details"></div>';
+      var hover_user_details = '<div class="hover-details"></div>';
       // Declaring the variable 'hoverUserDetails', it holds the initiating HTML tag useful for the Hovercard JS plugin.
       $("a.username").hovercard({
-      // Targeting 'hovercard' function for all anchor tags with 'username' as their class. This function will initiate the 'hovercard' function from the Hovercard JS file.
-        detailsHTML: hoverUserDetails,
+      // Targetting 'hovercard' function for all anchor tags with 'username' as their class. This function will initiate the 'hovercard' function from the Hovercard JS file.
+        detailsHTML: hover_user_details,
         width: 250,
         onHoverIn: function() {
           var base_path = Drupal.settings.basePath;
@@ -20,12 +20,12 @@
           var hover_details = $(".hover-details");
           // Declaring this as a variable for efficiency as its used multiple times.
           var user_id = $(this).find("a").attr("href").split("/");
-          // We'll store the User ID in this variable.
+          // We'll store the User ID in this variable
           $.ajax({
-            url: Drupal.settings.basePath + "hover-card/" + user_id[3],
+            url: base_path + "hover-card/" + user_id[3],
             beforeSend: function() {
               hover_details.empty();
-              hover_details.prepend('<p style="text-align: center"><img src="' + basePath + module_path + '/images/ajax-loader.gif"></p>');
+              hover_details.prepend('<p style="text-align: center"><img src="' + base_path + module_path + '/images/ajax-loader.gif"></p>');
             },
             success: function(data) {
               hover_details.html(data);
